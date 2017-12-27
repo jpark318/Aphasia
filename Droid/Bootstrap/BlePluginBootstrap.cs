5483df70b95198e043aa17545f61825797b3d267
Junbin Park <junbin.park@gmail.com>
2017-12-27 00:19:46 -0600

    package manage

---
+++ Droid/Bootstrap/BlePluginBootstrap.cs
@@ -0,0 +1,9 @@
+using MvvmCross.Platform.Plugins;
+
+namespace Aphasia.Droid.Bootstrap
+{
+    public class BlePluginBootstrap
+        : MvxPluginBootstrapAction<MvvmCross.Plugins.BLE.PluginLoader>
+    {
+    }
+}
---
+++ iOS/Bootstrap/BlePluginBootstrap.cs
@@ -0,0 +1,9 @@
+using MvvmCross.Platform.Plugins;
+
+namespace Aphasia.iOS.Bootstrap
+{
+    public class BlePluginBootstrap
+        : MvxLoaderPluginBootstrapAction<MvvmCross.Plugins.BLE.PluginLoader, MvvmCross.Plugins.BLE.iOS.Plugin>
+    {
+    }
+}
